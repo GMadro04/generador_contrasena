efb86d57dfa6487588546f0f8e48c3f364fa1b01
Gabriel Madriñero <gmadro0405@gmail.com>
2023-04-28 17:48:49 -0500

    Se extienden las listas de caracteres para mayor variedad y se automatiza la lista de minusculas

--- generador.py
+++ generador.py
@@ -1,8 +1,11 @@
 import random 
 
 # Creo unas primeras listas con caracteres para el generador 
-mayúsculas = ['A', 'B', 'C', 'D', 'F', 'G', 'H', 'I', 'J', 'K', 'L']
-minúsculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l']
+mayúsculas = ['A', 'B', 'C', 'D', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+              'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+# Se automatiza los cambios de mayusculas a minusculas 
+for i in range(len(mayúsculas)):
+    minúsculas = mayúsculas[i].lower
 symbols = ['*', '+', '-', '/', '@', '_', '?', '!', '[', '{', '(', ')']
 numero = ['1','2','3','4','5']
 
@@ -14,6 +17,7 @@ contraseña = []
 for i in range(10):
     select = random.choice(contraseña)
     contraseña.append(select)
-    contraseña = "".join(contraseña)
-    return contraseña
+contraseña = "".join(contraseña)
+
+print(contraseña)
     
