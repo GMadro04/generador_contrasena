97279da89dd1429c3b7f9e4c39e8f10c3b5bff4d
Gabriel Madriñero <gmadro0405@gmail.com>
2023-04-28 17:28:31 -0500

    Agrego codigo para generar la contraseña queda pendiente las funciones para pner el codigo a correr

--- generador.py
+++ generador.py
@@ -1,7 +1,19 @@
 import random 
 
 # Creo unas primeras listas con caracteres para el generador 
-mayusculas = ['A', 'B', 'C', 'D', 'F', 'G', 'H', 'I', 'J', 'K', 'L']
-minusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l']
-simbolos = ['*', '+', '-', '/', '@', '_', '?', '!', '[', '{', '(', ')']
-numeros = ['1','2','3','4','5']
+mayúsculas = ['A', 'B', 'C', 'D', 'F', 'G', 'H', 'I', 'J', 'K', 'L']
+minúsculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l']
+symbols = ['*', '+', '-', '/', '@', '_', '?', '!', '[', '{', '(', ')']
+numero = ['1','2','3','4','5']
+
+# Uno las listas para hacer una sola donde van a ser seleccionadas
+fusion = mayúsculas+minúsculas+symbols+numero
+# Creo una lista vacía para llenar la contraseña
+contraseña = []
+# Aquí va el ciclo con el que se arma la contraseña 
+for i in range(10):
+    select = random.choice(contraseña)
+    contraseña.append(select)
+    contraseña = "".join(contraseña)
+    return contraseña
+    
