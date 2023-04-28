b2817b2ed9cb6c423844ecdb51d00a175374e786
Gabriel Madriñero <gmadro0405@gmail.com>
2023-04-28 17:22:09 -0500

    Agregando listas con caracteres letras y simbolos para el generador

---
+++ generador.py
@@ -0,0 +1,7 @@
+import random 
+
+# Creo unas primeras listas con caracteres para el generador 
+mayusculas = ['A', 'B', 'C', 'D', 'F', 'G', 'H', 'I', 'J', 'K', 'L']
+minusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l']
+simbolos = ['*', '+', '-', '/', '@', '_', '?', '!', '[', '{', '(', ')']
+numeros = ['1','2','3','4','5']
